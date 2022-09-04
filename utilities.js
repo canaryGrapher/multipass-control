6f5a1382505d0786d097e0bc8c76add408c3b387
Davide Bertola <dade@dadeb.it>
2022-09-04 16:15:23 +0200

    fix spawn for recent node versions

--- utilities.js
+++ utilities.js
@@ -1,7 +1,7 @@
 const { spawnSync } = require("child_process");
 
 const run_command = (cmd, args) => {
-  return spawnSync(cmd, args);
+  return spawnSync(cmd, args, { shell: true });
 };
 
 // return each line as an array with each word separated
@@ -9,7 +9,7 @@ const output_split_array = (cmd) => {
   const command_output =
     process.platform === "win32"
       ? run_command("cmd.exe", ["/c", cmd, "--format", "json"])
-      : run_command(cmd);
+      : run_command(cmd + " --format json");
   return command_output.stdout.toString();
 };
 
